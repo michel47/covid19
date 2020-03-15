cfc45462c90ab8d88f122424b576fb3200fd6036
Quinton S. Alig <qalig@healthium.gq>
2020-03-15 14:53:00 +0100

    v0.4.4 14:52:40: update .md on 03/15/20 ~  . qm: QmeD2guU8wh8L6XdxboukDodFzvxBixWA8x2oehaasCDns tag: z25RnHUFy4PU6FHv43FgP4BtNzTJ

---
+++ daily.sh
@@ -0,0 +1,91 @@
+#
+
+domain=kintm.gq
+url=https://www.worldometers.info/coronavirus/
+mdfile=$HOME/github.com/covid19/covid19.md
+echo ${mdfile%/*}
+
+date=$(date +%D)
+tic=$(date +%s)
+qm=$(ipfs add -Q $url)
+echo tic: $tic
+echo url: https://ipfs.blockringtm.ml/ipfs/$qm
+echo "- [$date]: [$qm](https://cloudflare-ipfs.com/ipfs/$qm)" >> $mdfile;
+cat $mdfile | uniq > $mdfile~
+mv $mdfile~ $mdfile
+
+if [ -e covid.htm ]; then
+mtime=$(stat -c "%Y" covid.htm)
+if expr $tic - $mtime \> 21600; then
+ echo "info: tic - mtime > 21600"
+ rm covid.htm
+else
+ echo "reuse: codiv.htm $(stat -c '%y' covid.htm)"
+fi
+fi
+
+if [ ! -e covid.htm ]; then
+curl -s $url > covid.htm
+fi
+pandoc -f html -t json covid.htm > covid.json
+case=$(cat covid.json | xjson blocks.1.c.1.2.c.1.0.c.1.2.c.1.71.c.0.c)
+pandoc -f html -t markdown covid.htm > covid.md
+#data="$(echo '/Switzerland/+1,/Switzerland/+5p' | ed covid.md)"
+echo '/Switzerland/+1,/Switzerland/+7p' | ed covid.md > covid.dat
+if tail -1 covid.dat | grep -q "/^[+0-9]/"; then
+ tail -1 covid.dat
+paste -d' ' - covid.dat <<EOT | eyml > codiv.yml
+cases:
+deaths:
+recovered:
+active:
+densit:
+EOT
+else
+paste -d' ' - covid.dat <<EOT | eyml > codiv.yml
+cases:
+ncases:
+deaths:
+ndeaths:
+recovered:
+active:
+densit:
+EOT
+fi
+eval $(cat codiv.yml)
+echo "c:$cases d:$deaths r:$recovered a:$active d:$densit"
+
+cd ${mdfile%/*}
+
+eval $(perl -S fullname.pl -a $qm | eyml)
+git config user.name "$fullname"
+git config user.email $user@$domain
+echo "gituser: $(git config user.name) <$(git config user.email)>"
+
+git add $mdfile covid.md covid.json codiv.yml
+pandoc -f markdown -t html $HOME/github.com/covid19/covid19.md -o covid19.html
+qm=$(ipfs add -Q -w covid19.html)
+pwd
+cat > README.md <<EOF
+# README: corona virus daily status ...
+
+ $densit cases per 1M pop,\
+ $cases Total cases in Switzerland ($active actives)
+
+ $deaths deaths,
+ $recovered recovered
+
+last update : <https://ipfs.blockringtm.ml/ipfs/$qm/covid19.html>
+
+source:
+  - <https://www.bag.admin.ch/bag/fr/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/situation-schweiz-und-international.html>
+  - <https://twitter.com/BAG_OFSP_UFSP>
+  - <https://www.worldometers.info/coronavirus/>
+  - <https://duckduckgo.com/?q=switzerland+progression+coronavirus>
+  
+
+EOF
+git add README.md
+git commit -m "status on $date"
+git push
+echo $tic: $qm >> $HOME/etc/mutables/covid.log
